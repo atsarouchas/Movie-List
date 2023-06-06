62b2cccc8b9109251aaf04c4ffc121933a9c80c8
Anastasios Tsarouchas <anastasios.tsarouchas@gfk.com>
2023-06-06 22:55:27 +0300

    fix: revert url

--- src/services/searchMovies.ts
+++ src/services/searchMovies.ts
@@ -3,7 +3,7 @@ import { hideLoading } from '../libraries/dom/utils';
 export const searchMovies = async (query: string, pageNumber: number) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/search/modsdsdssvie?api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US&query=${query}&page=${pageNumber}&include_adult=false`
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US&query=${query}&page=${pageNumber}&include_adult=false`
     );
 
     const data = await response.json();
