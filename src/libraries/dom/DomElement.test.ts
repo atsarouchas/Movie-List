f767b8b53e1e6a7065adb78f4cb7326f7e70ed8d
Anastasios Tsarouchas <anastasios.tsarouchas@gfk.com>
2023-06-06 21:00:29 +0300

    feat: add favicon

---
+++ src/libraries/dom/DomElement.test.ts
@@ -0,0 +1,40 @@
+import { DomElement } from './DomElement';
+
+describe('DomElement', () => {
+  let domElement: DomElement;
+  let mockChildElement: HTMLElement;
+
+  beforeEach(() => {
+    domElement = new DomElement('div');
+    mockChildElement = document.createElement('span');
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('on method adds event listener to the element', () => {
+    const mockEventHandler = jest.fn();
+    domElement.on('click', mockEventHandler);
+    domElement.element.dispatchEvent(new Event('click'));
+    expect(mockEventHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it('appendElement method appends a child element', () => {
+    domElement.appendElement(mockChildElement);
+    expect(domElement.element.contains(mockChildElement)).toBeTruthy();
+  });
+
+  it('setAttribute method sets an attribute on the element', () => {
+    const attrName = 'data-testid';
+    const attrValue = 'test-id';
+    domElement.setAttribute(attrName, attrValue);
+    expect(domElement.element.getAttribute(attrName)).toBe(attrValue);
+  });
+
+  it('setInnerHtml method sets the inner HTML of the element', () => {
+    const innerHtml = '<p>Hello, World!</p>';
+    domElement.setInnerHtml(innerHtml);
+    expect(domElement.element.innerHTML).toBe(innerHtml);
+  });
+});
